4869522b90a5c80706d6e54ff0208fa7c8d4bcfa
sharabgithub <34997621+sharabgithub@users.noreply.github.com>
2024-12-26 12:37:04 +0530

    Update Base.java

--- src/main/java/com/example/Base.java
+++ src/main/java/com/example/Base.java
@@ -1,7 +1,7 @@
 package com.example;
 
 public class Base {
-
+//this is important initialize method!!!!
     public static void initializeDriver(){
         System.out.println("This is the initialize driver method");
     }
