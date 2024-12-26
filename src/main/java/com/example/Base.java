b60ecfbc82d3a5cceee6eeac47cba254cd67b481
sharab1github <hiprasannakumar.s@gmail.com>
2024-12-26 17:31:56 +0530

    Base pout msg changed

--- src/main/java/com/example/Base.java
+++ src/main/java/com/example/Base.java
@@ -3,7 +3,7 @@ package com.example;
 public class Base {
 //this is important initialize method!!!!
     public static void initializeDriver(){
-        System.out.println("This is the initialize driver method");
+        System.out.println("This is the initialize driver method --- Updated for Jenkins");
     }
     
 }
